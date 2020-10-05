2419d7d6f0a153d82d3fc149ba57e8b58c23d9ca
zlc <18229972186@163.com>
2020-10-05 12:31:25 +0800

    崩了很多次才完成的结果

---
+++ assets/js/article/art_list.js
@@ -0,0 +1,121 @@
+$(function() {
+    var layer = layui.layer
+    var form = layui.form
+    var laypage = layui.laypage
+
+    // 时间过滤器
+    template.defaults.imports.dataFormat = function(date) {
+        const dt = new Date(date)
+
+        var y = dt.getFullYear()
+        var m = padZero(dt.getMonth() + 1)
+        var d = padZero(dt.getDate())
+
+        var hh = padZero(dt.getHours())
+        var mm = padZero(dt.getMinutes())
+        var ss = padZero(dt.getSeconds())
+
+        return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+    }
+
+    // 补零
+    function padZero(n) {
+        return n > 9 ? n : '0' + n
+    }
+
+    var q = {
+        pagenum: 1, // 页码值
+        pagesize: 2, // 每页显示几条数据，默认2条
+        cate_id: '', // 文章分类的 Id
+        state: '' // 文章的发布状态
+    }
+
+    initTable()
+    initCate()
+
+    // 获取文章列表数据
+    function initTable() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取文章列表失败！')
+                }
+
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+                renderPage(res.total)
+            }
+        })
+    }
+
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取分类数据失败！')
+                }
+                var htmlStr = template('tpl-cate', res)
+                $('[name=cate_id]').html(htmlStr)
+                form.render()
+            }
+        })
+    }
+
+    $('#form-search').on('submit', function(e) {
+        e.preventDefault()
+        var cate_id = $('[name=cate_id]').val()
+        var state = $('[name=state]').val()
+        q.cate_id = cate_id
+        q.state = state
+        initTable()
+    })
+
+    function renderPage(total) {
+        laypage.render({
+            elem: 'pageBox', // 分页容器的 Id
+            count: total, // 总数据条数
+            limit: q.pagesize, // 每页显示几条数据
+            curr: q.pagenum, // 设置默认被选中的分页
+            layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+            limits: [2, 3, 5, 10],
+            jump: function(obj, first) {
+                console.log(first)
+                console.log(obj.curr)
+                q.pagenum = obj.curr
+                q.pagesize = obj.limit
+                if (!first) {
+                    initTable()
+                }
+            }
+        })
+    }
+
+    $('tbody').on('click', '.btn-delete', function() {
+        var len = $('.btn-delete').length
+        console.log(len)
+        var id = $(this).attr('data-id')
+        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/delete/' + id,
+                success: function(res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除文章失败！')
+                    }
+                    layer.msg('删除文章成功！')
+                    if (len === 1) {
+                        q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1
+                    }
+                    initTable()
+                }
+            })
+
+            layer.close(index)
+        })
+    })
+})
